79e648b82b40b0acc25749c2d7bf574fba456d2c
Francisco Jr. B. Cenci <124214803+franciscocenci@users.noreply.github.com>
2025-09-23 01:13:03 +0000

    build: install `next-connect`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -33,11 +33,11 @@ async function status(request, response) {
       },
     });
   } catch (error) {
-    const publicErrorObject = new InternalServerError({
+    const publicErrorObject = new InternalServgiterError({
       cause: error,
     });
 
-    console.log("\n Erro dentro do catah do controller:");
+    console.log("\n Erro dentro do catch do controller:");
     console.error(publicErrorObject);
 
     response.status(500).json(publicErrorObject);
